e338b608d513bcf6f6297468c6e18545ea2718e0
Pratik Patwari <pratikpatwari24@gmail.com>
2022-07-24 15:34:06 +0530

    Learning project

---
+++ backend/seeder.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import users from './data/users.js'
+import products from './data/products.js'
+
+dotenv.config()
+connectDB()
+
+const importData = async () => {
+    try{
+        await User.deleteMany()
+        await Product.deleteMany()
+        await Order.deleteMany()
+
+        const createdUsers = await User.insertMany(users)
+        const adminID = createdUsers[0]._id
+
+        const sampleProduct = products.map((product) => { return { ...product, user: adminID}})
+
+        await Product.insertMany(sampleProduct)
+
+        console.log(`Data imported!`.green.inverse)
+        process.exit()
+    } catch(error){
+        console.error(`Error: ${error}`.red.inverse);
+        process.exit(1)
+    }
+} 
+
+const destroyData = async () => {
+    try {
+      await Order.deleteMany()
+      await Product.deleteMany()
+      await User.deleteMany()
+  
+      console.log('Data Destroyed!'.red.inverse)
+      process.exit()
+    } catch (error) {
+      console.error(`${error}`.red.inverse)
+      process.exit(1)
+    }
+}
+
+if(process.argv[2] === '-d'){
+    destroyData()
+} else {
+    importData()
+}
